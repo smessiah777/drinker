4d2c1a695fc74b735b81117712439522c732e4b3
Stephen Messiah <smessiahh17@gmail.com>
2022-12-09 20:50:09 -0600

    update Search section bg style to have min 100vh and auto default

--- src/components/landing/Search.tsx
+++ src/components/landing/Search.tsx
@@ -1,12 +1,14 @@
 import DrinkList from "../drinks/DrinkList"
 
+
 const Search = () => {
         
     
     
 return(
-        <section id="search" className="bg-white bg-drinks-bg bg-cover bg-no-repeat lg:h-screen xs:h-auto xs:pb-5">
+        <section id="search" className="bg-white bg-drinks-bg bg-cover bg-no-repeat lg:min-h-screen lg:h-auto xs:h-auto xs:pb-5">
             <div  className="flex flex-col items-center pt-5">
+                
                 <DrinkList />
             </div>
 
