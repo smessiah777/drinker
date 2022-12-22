bb995a1afbe65cebf463a7b8bb3955f65831ed9c
Stephen Messiah <smessiahh17@gmail.com>
2022-12-21 20:22:26 -0600

    created next custom document and added google font style link. updated faveicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { ReactQueryDevtools } from 'react-query/devtools'
 import Hero from '../components/landing/Hero'
 import Search from '../components/landing/Search'
@@ -12,14 +11,14 @@ import Search from '../components/landing/Search'
 const Home: NextPage = () => {
  return (
     <>
-      {/* <ReactQueryDevtools initialIsOpen={false}/> */}
+      
      
      
       <Head>
         <title>Drinker</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Unica+One&display=swap" rel="stylesheet"></link>
+        
       </Head>
 
 
